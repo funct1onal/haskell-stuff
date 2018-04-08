2b09fc498dde4e0c5df4bbd2ebc71d05c4f2a9cf
Misaka desu <24862715+funct1onal@users.noreply.github.com>
2018-04-07 19:11:27 -0700

    Update fzbz.hs
    made it neater

--- fzbz.hs
+++ fzbz.hs
@@ -3,8 +3,8 @@ module Main where
 
 fisubusu :: Int -> String
 fisubusu n | n % 15 == 0 == "FizzBuzz"
-fisubusu n | n % 3 == 0 == "Buzz"
 fisubusu n | n % 5 == 0 == "Fizz"
+fisubusu n | n % 3 == 0 == "Buzz"
            | otherwise = show n
 
 
