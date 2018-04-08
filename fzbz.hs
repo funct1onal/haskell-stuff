96cc48ada295f8ce56877d6ac2d1fcdf6f3d8217
Misaka desu <24862715+funct1onal@users.noreply.github.com>
2018-04-07 19:08:17 -0700

    haskell fizzbuzz

---
+++ fzbz.hs
@@ -0,0 +1,15 @@
+module Main where
+
+
+fisubusu :: Int -> String
+fisubusu n | n % 15 == 0 == "FizzBuzz"
+fisubusu n | n % 3 == 0 == "Buzz"
+fisubusu n | n % 5 == 0 == "Fizz"
+           | otherwise = show n
+
+
+main :: IO ()
+main = printAll $ map Fisubusu [1..100]
+    where
+    printAll [] = return ()
+    printAll (x:xy) = putStrLn x >> printAll xy
